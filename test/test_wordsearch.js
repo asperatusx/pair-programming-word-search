ca22f985949492625fffd6cb945b8ca231e23191
Andrew <andrewlang.p@gmail.com>
2024-10-24 01:56:16 -0400

    refactor the function and add more test cases

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,61 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return true if the vertical word is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'A', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'N', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'D', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'R', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'E', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'W', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'ANDREW')
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the backword horizontal word is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'A', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'N', 'V', 'R', 'G'],
+      ['W', 'E', 'R', 'D', 'N', 'A', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'R', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'Z', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'W', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'ANDREW')
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if there is empty word or string", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'A', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'N', 'V', 'R', 'G'],
+      ['W', 'E', 'R', 'D', 'N', 'A', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'R', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'Z', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'W', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], '')
+
+    assert.isFalse(result);
+  });
+  
+  it("should return false if the array is empty", function() {
+    const result = wordSearch([], 'ANDREW')
+    assert.isFalse(result);
+  });
+
+
+  
+
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,27 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  if (!letters.length || !word) return false
+  if (findHorizontalWord(letters, word)) return true
+  let rowArray = [];
+  let VerticalArray = [];
+  for (let ele = 0; ele < letters[0].length; ele++) {
+    rowArray = [];
+    for (let arr = 0; arr < letters.length; arr++) {
+      rowArray.push(letters[arr][ele]);
     }
+    VerticalArray.push(rowArray);
+  }
+  if (findHorizontalWord(VerticalArray, word)) {
+    return true
+  }
+return false
+}
+
+const findHorizontalWord = (array, word) => {
+  const newArray = array.map(ls => ls.join(''));
+  const reversedWord = word.split('').reverse().join('');
+  for (l of newArray) {
+    if (l.includes(word) || l.includes(reversedWord)) return true
+  }
 }
 
 module.exports = wordSearch
